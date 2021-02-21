45ddbb33426d1dceb286512bd71447ce6a0791cf
elizchan <lizchan@bu.edu>
2021-02-21 17:29:40 -0500

    auth.interceptor file component is not recognized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,15 @@ import { MenuComponent } from './components/menu/menu.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { Auth } from './_helpers/auth.interceptor/auth.interceptor.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +25,14 @@ import { RecipeDetailsComponent } from './components/recipe-details/recipe-detai
     MenuComponent,
     RecipeListComponent,
     AddRecipeComponent,
-    RecipeDetailsComponent
+    RecipeDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardUserComponent,
+    Auth.InterceptorComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +40,7 @@ import { RecipeDetailsComponent } from './components/recipe-details/recipe-detai
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
